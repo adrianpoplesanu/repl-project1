8d5a77d414a9606ce43f119c6bd2a0fade1ced70
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2021-04-10 09:37:52 +0300

    simple login function

--- web/sandbox/src/app/adrianus/views.py
+++ web/sandbox/src/app/adrianus/views.py
@@ -2,5 +2,20 @@ from django.shortcuts import render
 
 
 def home(request):
-    env = {}
+    env = {
+    	'is_logged_in': False,
+    	'username': None,
+    	'userid': None,
+    	'name': None
+    }
     return render(request, 'home.html', env)
+
+
+def guest_login(request):
+	env = {
+		'is_logged_in': True,
+		'usermame': 'Guest215486',
+		'userid': -1,
+		'name': 'Guest215486'
+	}
+	return render(request, 'guest-login.html', env)
--- web/sandbox/src/app/app/urls.py
+++ web/sandbox/src/app/app/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from check2.views import check2
-from adrianus.views import home
+from adrianus.views import home, guest_login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('check2.txt', check2),
-    path('', home)
+    path('', home),
+    path('guest-login', guest_login)
 ]
