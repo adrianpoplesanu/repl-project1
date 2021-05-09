a8cb98d4f7d6ae36ecbc908899226c49fb914c0a
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-05-09 18:36:52 +0300

    lexer implementation

--- repl/adrianus/cpp/lexer.cpp
+++ repl/adrianus/cpp/lexer.cpp
@@ -9,55 +9,122 @@ Lexer::~Lexer() {
 }
 
 void Lexer::Load(std::string s) {
-	source = s;
+    source = s;
 }
 
 void Lexer::ReadChar() {
-	if (readPosition < source.length()) {
-		current_char = source[readPosition];
-	} else {
-		current_char = 0;
-	}
-	position = readPosition;
-	++readPosition;
+    if (readPosition < source.length()) {
+        current_char = source[readPosition];
+    } else {
+        current_char = 0;
+    }
+    position = readPosition;
+    ++readPosition;
 }
 
 void Lexer::SkipWhitespace() {
-	if (current_char == ' ' || current_char == '\t' || current_char == '\r' || current_char == '\n') {
-		ReadChar();
-	}
+    while (current_char == ' ' || current_char == '\t' || current_char == '\r' || current_char == '\n') {
+        ReadChar();
+    }
 }
 
 bool Lexer::IsLetter() {
-	return (('a' <= current_char) && (current_char <= 'z')) || (('A' <= current_char) && (current_char <= 'Z')) || current_char == '_';
+    return (('a' <= current_char) && (current_char <= 'z')) || (('A' <= current_char) && (current_char <= 'Z')) || current_char == '_';
 }
 
 bool Lexer::IsDigit() {
-	return ('0' <= current_char) && (current_char <= '9');
+    return ('0' <= current_char) && (current_char <= '9');
 }
 
 std::string Lexer::ReadIdentifier() {
-	// TODO
-	return "";
+    // TODO
+    return "";
 }
 
 std::string Lexer::ReadNumber() {
-	// TODO
-	return "";
+    // TODO
+    return "";
 }
 
 TokenType Lexer::LookupIdent(std::string ident) {
-	// TODO
-	return TT_ILLEGAL;
+    // TODO
+    return TT_ILLEGAL;
 }
 
 char Lexer::PeekChar() {
-	// TODO
-	return 0;
+    // TODO
+    return 0;
 }
 
 Token Lexer::NextToken() {
-	// TODO
-	Token t("", TT_ILLEGAL);
-	return t;
+    Token token;
+    SkipWhitespace();
+    switch(current_char) {
+        case 0:
+            token.type = TT_EOF;
+            token.literal = "";
+        break;
+        case '+':
+            token.type = TT_PLUS;
+            token.literal = current_char;
+        break;
+        case '-':
+            token.type = TT_MINUS;
+            token.literal = current_char;
+        break;
+        case '*':
+            token.type = TT_ASTERISK;
+            token.literal = current_char;
+        break;
+        case '/':
+            token.type = TT_SLASH;
+            token.literal = current_char;
+        break;
+        case '<':
+            token.type = TT_LT;
+            token.literal = current_char;
+        break;
+        case '>':
+            token.type = TT_GT;
+            token.literal = current_char;
+        break;
+        case ',':
+            token.type = TT_COMMA;
+            token.literal = current_char;
+        break;
+        case ';':
+            token.type = TT_SEMICOLON;
+            token.literal = current_char;
+        break;
+        case '(':
+            token.type = TT_LPAREN;
+            token.literal = current_char;
+        break;
+        case ')':
+            token.type = TT_RPAREN;
+            token.literal = current_char;
+        break;
+        case '{':
+            token.type = TT_LBRACE;
+            token.literal = current_char;
+        break;
+        case '}':
+            token.type = TT_RBRACE;
+            token.literal = current_char;
+        break;
+        case '=':
+        break;
+        case '!':
+        break;
+        default:
+            if (IsLetter()) {
+
+            }
+            if (IsDigit()) {
+
+            }
+        break;
+    }
+    ReadChar();
+    return token;
 }
--- repl/adrianus/cpp/token.h
+++ repl/adrianus/cpp/token.h
@@ -5,85 +5,85 @@
 #include <map>
 
 enum TokenType {
-	TT_ILLEGAL,
-	TT_EOF_FLAG,
-	TT_IDENT,
-	TT_ASSIGN,
-	TT_LPAREN,
-	TT_RPAREN,
-	TT_LBRACE,
-	TT_RBRACE,
-	TT_LT,
-	TT_GT,
-	TT_EQ,
-	TT_NOT_EQ,
-	TT_IF,
-	TT_ELSE,
-	TT_RETURN,
-	TT_SEMICOLON,
-	TT_COMMA,
-	TT_PLUS,
-	TT_MINUS,
-	TT_SLASH,
-	TT_ASTERISK,
-	TT_BANG,
-	TT_TRUE_VALUE,
-	TT_FALSE_VALUE,
-	TT_FUNCTION,
-	TT_LET
+    TT_ILLEGAL,
+    TT_EOF,
+    TT_IDENT,
+    TT_ASSIGN,
+    TT_LPAREN,
+    TT_RPAREN,
+    TT_LBRACE,
+    TT_RBRACE,
+    TT_LT,
+    TT_GT,
+    TT_EQ,
+    TT_NOT_EQ,
+    TT_IF,
+    TT_ELSE,
+    TT_RETURN,
+    TT_SEMICOLON,
+    TT_COMMA,
+    TT_PLUS,
+    TT_MINUS,
+    TT_SLASH,
+    TT_ASTERISK,
+    TT_BANG,
+    TT_TRUE,
+    TT_FALSE,
+    TT_FUNCTION,
+    TT_LET
 };
 
 std::map<TokenType, std::string> token_type_map = {
-	{TT_ASSIGN, "ASSIGN"},
-	{TT_ILLEGAL, "ILLEGAL"},
-	{TT_EOF_FLAG, "EOF"},
-	{TT_TRUE_VALUE, "TRUE"},
-	{TT_FALSE_VALUE, "FALSE"},
-	{TT_IDENT, "IDENT"},
-	{TT_FUNCTION, "FUNCTION"},
-	{TT_LET, "LET"},
-	{TT_LPAREN, "LPAREN"},
-	{TT_RPAREN, "RPAREN"},
-	{TT_LBRACE, "LBRACE"},
-	{TT_RBRACE, "RBRACE"},
-	{TT_LT, "LT"},
-	{TT_GT, "GT"},
-	{TT_EQ, "EQ"},
-	{TT_NOT_EQ, "NOT_EQ"},
-	{TT_IF, "IF"},
-	{TT_ELSE, "ELSE"},
-	{TT_RETURN, "RETURN"},
-	{TT_SEMICOLON, "SEMICOLON"},
-	{TT_COMMA, "COMMA"},
-	{TT_PLUS, "PLUS"},
-	{TT_MINUS, "MINUS"},
-	{TT_SLASH, "SLASH"},
-	{TT_ASTERISK, "ASTERISK"},
-	{TT_BANG, "BANG"}
+    {TT_ASSIGN, "ASSIGN"},
+    {TT_ILLEGAL, "ILLEGAL"},
+    {TT_EOF, "EOF"},
+    {TT_TRUE, "TRUE"},
+    {TT_FALSE, "FALSE"},
+    {TT_IDENT, "IDENT"},
+    {TT_FUNCTION, "FUNCTION"},
+    {TT_LET, "LET"},
+    {TT_LPAREN, "LPAREN"},
+    {TT_RPAREN, "RPAREN"},
+    {TT_LBRACE, "LBRACE"},
+    {TT_RBRACE, "RBRACE"},
+    {TT_LT, "LT"},
+    {TT_GT, "GT"},
+    {TT_EQ, "EQ"},
+    {TT_NOT_EQ, "NOT_EQ"},
+    {TT_IF, "IF"},
+    {TT_ELSE, "ELSE"},
+    {TT_RETURN, "RETURN"},
+    {TT_SEMICOLON, "SEMICOLON"},
+    {TT_COMMA, "COMMA"},
+    {TT_PLUS, "PLUS"},
+    {TT_MINUS, "MINUS"},
+    {TT_SLASH, "SLASH"},
+    {TT_ASTERISK, "ASTERISK"},
+    {TT_BANG, "BANG"}
 };
 
 std::map<std::string, TokenType> keywords = {
-	{"if", TT_IF},
-	{"else", TT_ELSE},
-	{"return", TT_RETURN},
-	{"true", TT_TRUE_VALUE},
-	{"false", TT_FALSE_VALUE},
-	{"fn", TT_FUNCTION},
-	{"let", TT_LET}
+    {"if", TT_IF},
+    {"else", TT_ELSE},
+    {"return", TT_RETURN},
+    {"true", TT_TRUE},
+    {"false", TT_FALSE},
+    {"fn", TT_FUNCTION},
+    {"let", TT_LET}
 };
 
 class Token {
-private:
-	std::string literal;
-	TokenType type;
 public:
-	Token();
-	Token(std::string, TokenType);
-	~Token();
-	std::string GetLiteral();
-	void SetLiteral(std::string);
-	TokenType GetType();
-	void SetType(TokenType);
+    std::string literal;
+    TokenType type;
+
+    Token();
+    Token(std::string, TokenType);
+    ~Token();
+    std::string GetLiteral();
+    void SetLiteral(std::string);
+    TokenType GetType();
+    void SetType(TokenType);
 };
 
 #endif
