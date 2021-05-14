06dc7006774541053f34eae5faad398f54312786
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-05-14 23:10:05 +0300

    ast nodes and remove debug flag

--- repl/adrianus/cpp/ast.cpp
+++ repl/adrianus/cpp/ast.cpp
@@ -1,2 +1,33 @@
 #include "ast.h"
 
+std::string Ad_AST_Node::TokenLiteral() {
+    return "node literal - implement in subclass";
+}
+
+std::string Ad_AST_Node::String() {
+    return "node string repr - implement in subclass";
+}
+
+std::string Ad_AST_Statement::TokenLiteral() {
+    return "todo - implement this";
+}
+
+std::string Ad_AST_Statement::String() {
+    return "todo - implement this";
+}
+
+std::string Ad_AST_Expression::TokenLiteral() {
+    return "todo - implement this";
+}
+
+std::string Ad_AST_Expression::String() {
+    return "todo - implement this";
+}
+
+std::string Ad_AST_Program::TokenLiteral() {
+    return "todo - implement this";
+}
+
+std::string Ad_AST_Program::String() {
+    return "todo - implement this";
+}
--- repl/adrianus/cpp/ast.h
+++ repl/adrianus/cpp/ast.h
@@ -1,8 +1,33 @@
 #ifndef __AST_H
 #define __AST_H
 
-class Ad_AST_Node {
+#include <string>
+#include <vector>
 
+class Ad_AST_Node {
+public:
+    std::string TokenLiteral();
+    std::string String();
+};
+
+class Ad_AST_Statement : public Ad_AST_Node {
+public:
+    std::string TokenLiteral();
+    std::string String();
+};
+
+class Ad_AST_Expression : public Ad_AST_Node {
+public:
+    std::string TokenLiteral();
+    std::string String();
+};
+
+class Ad_AST_Program : public Ad_AST_Node {
+public:
+    std::vector<Ad_AST_Node*> statements;
+
+    std::string TokenLiteral();
+    std::string String();
 };
 
 #endif
