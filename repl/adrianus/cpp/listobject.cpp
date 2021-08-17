dbac4acde18558e7267eb1aacb2ddaf8300d989d
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-08-18 00:57:17 +0300

    added stubs for list object

--- repl/adrianus/cpp/listobject.cpp
+++ repl/adrianus/cpp/listobject.cpp
@@ -10,3 +10,53 @@ Ad_List::~Ad_List() {
     for (int i = 0; i < size; i++) free_Ad_Object_memory(data[i]);
     delete[] data;
 }
+
+Ad_Object* Ad_List::Get(int i) {
+    if (!IsValidBounds(i)) return NULL;
+    return data[i];
+}
+
+void Ad_List::Set(Ad_Object* obj, int i) {
+    if (IsValidBounds(i)) {
+        data[i] = obj;
+    }
+}
+
+void Ad_List::Add(Ad_Object* obj, int i) {
+
+}
+
+Ad_Object* Ad_List::Remove(int i) {
+    return NULL;
+}
+
+int Ad_List::Size() {
+    return size;
+}
+
+bool Ad_List::IsEmpty() {
+    return size == 0;
+}
+
+void Ad_List::Reverse() {
+
+}
+
+void Ad_List::Sort() {
+
+}
+
+void Ad_List::Resize(int n) {
+
+}
+
+void Ad_List::Append(Ad_Object* obj) {
+
+}
+
+bool Ad_List::IsValidBounds(int i) {
+    if (i < 0 || i > size) {
+        return false;
+    }
+    return true;
+}
--- repl/adrianus/cpp/listobject.h
+++ repl/adrianus/cpp/listobject.h
@@ -4,21 +4,26 @@
 #include "objects.h"
 
 class Ad_List {
-public:
+private:
     Ad_Object **data;
     int capacity;
     int size;
-
+public:
     Ad_List();
     ~Ad_List();
-    Ad_Object* Get(int i);
-    void Set(Ad_Object*, int i);
-    void Add(Ad_Object*, int i);
-    void Remove(int i);
+    Ad_Object* Get(int);
+    void Set(Ad_Object*, int);
+    void Add(Ad_Object*, int);
+    Ad_Object* Remove(int);
     int Size();
+    int Capacity();
     bool IsEmpty();
     void Reverse();
     void Sort();
+    void Resize(int);
+    void Append(Ad_Object*);
+
+    bool IsValidBounds(int);
 };
 
 #endif
