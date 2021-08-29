56d50bc114f66a93f4e80f84af0f632812e6bd20
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-08-29 19:51:44 +0300

    environment implementation

--- repl/adrianus/java/ad/src/com/ad/environment/Environment.java
+++ repl/adrianus/java/ad/src/com/ad/environment/Environment.java
@@ -1,5 +1,45 @@
 package com.ad.environment;
 
-public class Environment {
+import java.util.HashMap;
 
+import com.ad.objects.AdObject;
+
+public class Environment {
+	private Environment outer;
+	public static HashMap<String, AdObject> store;
+	
+	public Environment() {
+		
+	}
+	
+	public Environment(Environment outer) {
+		this.outer = outer;
+	}
+	
+	public boolean check(String key) {
+		if (store.containsKey(key)) return true;
+		return false;
+	}
+	
+	public AdObject get(String key) {
+		if (store.containsKey(key)) {
+			return store.get(key);
+		}
+		if (outer.check(key)) return outer.get(key);
+		return null;
+	}
+	
+	public void set() {
+		
+	}
+	
+	public static Environment newEnvironment() {
+		Environment env = new Environment();
+		return env;
+	}
+	
+	public static Environment newEnclosedEnvironment(Environment outer) {
+		Environment env = new Environment(outer);
+		return env;
+	}
 }
