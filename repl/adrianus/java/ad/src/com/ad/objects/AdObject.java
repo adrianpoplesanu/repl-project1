4e6e43f29c5ff1159f9fb81ac7b5f4c5738e541e
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-09-02 09:19:34 +0300

    AdObject abstract class implementation and started work on the derived classes

--- repl/adrianus/java/ad/src/com/ad/objects/AdObject.java
+++ repl/adrianus/java/ad/src/com/ad/objects/AdObject.java
@@ -2,7 +2,15 @@ package com.ad.objects;
 
 public abstract class AdObject {
 	protected ObjectTypeEnum type;
+	
 	public abstract String inspect();
-	public abstract ObjectTypeEnum Type();
 	public abstract void print();
+	
+	public ObjectTypeEnum getType() {
+		return type;
+	}
+	
+	public void setType(ObjectTypeEnum type) {
+		this.type = type;
+	}
 }
