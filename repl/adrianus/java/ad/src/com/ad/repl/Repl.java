d8b6956954f3bcd0ae728470848ba5337eedd80c
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-09-04 01:50:02 +0300

    repl update to invoke eval with the proper env

--- repl/adrianus/java/ad/src/com/ad/repl/Repl.java
+++ repl/adrianus/java/ad/src/com/ad/repl/Repl.java
@@ -3,16 +3,20 @@ package com.ad.repl;
 import java.util.Scanner;
 
 import com.ad.ast.AstProgram;
+import com.ad.environment.Environment;
+import com.ad.evaluator.Evaluator;
 import com.ad.parser.Parser;
 
 public class Repl {
 	private Scanner scanner = new Scanner(System.in);
 	private Parser parser;
 	private AstProgram program;
+	private Evaluator evaluator;
 	
-	public Repl(Parser parser, AstProgram program) {
+	public Repl(Parser parser, AstProgram program, Evaluator evaluator) {
 		this.parser = parser;
 		this.program = program;
+		this.evaluator = evaluator;
 	}
 	
     public String getNextLine() {
@@ -20,11 +24,13 @@ public class Repl {
     }
     
     public void loop() {
+    	Environment env = Environment.newEnvironment();
     	while(true) {
     		String source = getNextLine();
     		if (source.equals("exit()")) break;
     		parser.load(source);
     		parser.buildProgramStatements(program);
+    		this.evaluator.eval(program, env);
     	}
     }
 }
