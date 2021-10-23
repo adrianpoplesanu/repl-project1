505cbb1a2111125e77c5b842c749c3bda23b3aeb
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2021-10-24 00:49:00 +0300

    functionality for executing Ad source files

--- repl/adrianus/java/ad/src/Ad.java
+++ repl/adrianus/java/ad/src/Ad.java
@@ -1,3 +1,7 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import com.ad.ast.AstProgram;
 import com.ad.evaluator.Evaluator;
 import com.ad.parser.Parser;
@@ -7,15 +11,26 @@ import com.ad.repl.Repl;
 public class Ad {
 
 	public static void main(String[] args) {
-		System.out.println("Ad intepreter [JAVA]... v1.0");
-
-        ParserTest.testParser();
-
-		Parser parser = new Parser();
-		AstProgram program = new AstProgram();
-		Evaluator evaluator = new Evaluator();
-
-		Repl repl = new Repl(parser, program, evaluator);
-		repl.loop();
+		if (args.length > 0) {
+			String filename = args[0];
+			String text;
+			try {
+				text = new String(Files.readAllBytes(Paths.get(filename)));
+			} catch (IOException e) {
+				text = "";
+			}
+			System.out.println(text);
+		} else {
+			System.out.println("Ad intepreter [JAVA]... v1.0");
+			
+	        ParserTest.testParser();
+	
+			Parser parser = new Parser();
+			AstProgram program = new AstProgram();
+			Evaluator evaluator = new Evaluator();
+	
+			Repl repl = new Repl(parser, program, evaluator);
+			repl.loop();
+		}
 	}
 }
