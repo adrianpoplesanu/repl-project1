6fbb24659c144ef6b461c406fc5dc4c2494d3292
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-02-11 23:37:29 +0200

    [java] AdFileObject

--- repl/adrianus/java/ad/src/com/ad/builtin/IOFileBuiltin.java
+++ repl/adrianus/java/ad/src/com/ad/builtin/IOFileBuiltin.java
@@ -1,13 +1,17 @@
 package com.ad.builtin;
 
 import com.ad.environment.Environment;
+import com.ad.objects.AdFileObject;
 import com.ad.objects.AdObject;
+import com.ad.objects.AdStringObject;
 
 import java.util.ArrayList;
 
 public class IOFileBuiltin implements BuiltinInterface {
     @Override
     public AdObject call(ArrayList<AdObject> args, Environment env) {
-        return null;
+        AdStringObject filenameObject = (AdStringObject) args.get(0);
+        AdStringObject operatorObject = (AdStringObject) args.get(1);
+        return new AdFileObject(filenameObject.getValue(), operatorObject.getValue());
     }
 }
