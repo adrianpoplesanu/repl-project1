c02a69ba79c15e82bb9bb3e2c54b942677ca6d7d
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-03-24 23:18:46 +0200

    [java] removed unused interfaces

--- repl/adrianus/java/ad/src/com/ad/parser/InfixParseInterface.java
+++
@@ -1,7 +0,0 @@
-package com.ad.parser;
-
-import com.ad.ast.AstNode;
-
-public interface InfixParseInterface {
-	AstNode parse(AstNode node);
-}
--- repl/adrianus/java/ad/src/com/ad/parser/PrefixParseInterface.java
+++
@@ -1,7 +0,0 @@
-package com.ad.parser;
-
-import com.ad.ast.AstNode;
-
-public interface PrefixParseInterface {
-	AstNode parse();
-}
