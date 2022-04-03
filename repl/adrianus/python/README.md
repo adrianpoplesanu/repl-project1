c788fb7f7ce1c8aea3b4b5f10bd1e46a2d750dd3
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-04-03 10:33:13 +0300

    removed old python 2.7 folder

--- repl/adrianus/python/README.md
+++
@@ -1,4 +0,0 @@
-Python implementation of Ad
-
-this implementation is used for prototyping
-once features are implemented here, they are developed in cpp and java
--- repl/adrianus/python/hash_utils.py
+++
@@ -1,8 +0,0 @@
-class HashKey(object):
-    def __init__(self, type=None, value=None):
-        """
-        @param: type - ObjectType
-        @param: value - hash int, ex: 3943517107174772026
-        """
-        self.type = type
-        self.value = value
