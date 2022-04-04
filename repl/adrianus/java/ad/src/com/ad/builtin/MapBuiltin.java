faa7073c015e529988498c919a6e186df8cd1baf
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-04-04 19:20:13 +0300

    [java] stub for map builtin

---
+++ repl/adrianus/java/ad/src/com/ad/builtin/MapBuiltin.java
@@ -0,0 +1,14 @@
+package com.ad.builtin;
+
+import com.ad.environment.Environment;
+import com.ad.objects.AdObject;
+
+import java.util.List;
+
+public class MapBuiltin implements BuiltinInterface {
+
+    @Override
+    public AdObject call(List<AdObject> args, Environment env) {
+        return null;
+    }
+}
