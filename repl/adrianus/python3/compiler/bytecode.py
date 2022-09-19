08f2101797f1095ad9c16b058fb53b2fed4a6129
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-09-19 11:14:40 +0300

    [python] placeholder for compiler and bytecode modules

--- repl/adrianus/python3/compiler/bytecode.py
+++ repl/adrianus/python3/compiler/bytecode.py
@@ -1,2 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath('../code'))
+
 class Bytecode:
-    pass
+    def __init__(self):
+        self.instructions = Instructions()
+        self.constants = []
--- repl/adrianus/python3/compiler/compiler.py
+++ repl/adrianus/python3/compiler/compiler.py
@@ -1,2 +1,24 @@
+import sys
+import os
+sys.path.append(os.path.abspath('../code'))
+
+from instructions import Instructions
+
 class Compiler:
-    pass
+    def __init__(self):
+        self.instructions = Instructions()
+        constants = []
+
+    def new(self):
+        self.instructions = Instructions()
+        constants = []
+
+    def compile(self, node):
+        # AstNode node
+        return None
+
+    def bytecode(self):
+        return Bytecode()
+
+if __name__ == '__main__':
+    compiler = Compiler()
