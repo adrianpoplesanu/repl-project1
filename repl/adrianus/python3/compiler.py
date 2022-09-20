d64530dc0aea7698823f54ad4853a1ee6dc988bd
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-09-20 23:40:24 +0300

    [python] compile() and add_constant()

--- repl/adrianus/python3/compiler.py
+++ repl/adrianus/python3/compiler.py
@@ -1,4 +1,5 @@
 from code.instructions import Instructions
+from objects import Ad_Integer_Object
 
 
 class Compiler:
@@ -12,11 +13,23 @@ class Compiler:
 
     def compile(self, node):
         if node.type == StatementType.PROGRAM:
-            pass
+            for stmt in node.statements:
+                self.compile(stmt)
+        elif node.type == StatementType.EXPRESSION_STATEMENT:
+            self.compile(node.expression)
+        elif node.type == StatementType.INFIX_EXPRESSION:
+            self.compile(node.left)
+            self.compile(node.right)
+        elif node.type == StatementType.INTEGER:
+            value = Ad_Integer_Object(node.value)
         return None
 
     def bytecode(self):
         return Bytecode()
+
+    def add_constant(self, obj):
+        self.constants.append(obj)
+        return len(self.constants) - 1
 
 if __name__ == '__main__':
     compiler = Compiler()
