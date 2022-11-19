af45028ed5714030d5a94cc490f08495540de96f
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-11-19 02:02:45 +0200

    [cpp] added dedicated garbage collection functionality

---
+++ repl/adrianus/cpp/gc.h
@@ -0,0 +1,19 @@
+#include "environment.h"
+#include "objects.h"
+#include "ast.h"
+
+class GarbageCollector {
+public:
+    std::vector<Environment*> garbage_collect_environments;
+    std::vector<Ad_Object*> garbage_collect_objects;
+    std::vector<Ad_AST_Node*> garbage_collect_ast_nodes;
+
+    GarbageCollector();
+    ~GarbageCollector();
+    void markEnvironment(Environment*);
+    void markObject(Ad_Object*);
+    void markAstNode(Ad_AST_Node*);
+    void sweepEnvironments();
+    void sweepObjects();
+    void sweepAstNodes();
+}
