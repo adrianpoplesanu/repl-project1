3575692dfc6b8e32a31553c19747bc405cc5caa6
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-12-05 00:46:30 +0200

    [it] updated test list to format testXXX.ad for cpp

--- repl/adrianus/integration-tests/cpp/test_proc_run_leaks.py
+++ repl/adrianus/integration-tests/cpp/test_proc_run_leaks.py
@@ -3,16 +3,16 @@ import subprocess
 binary_path = '../../cpp/'
 binary_excutable = 'main'
 
-target = 'examples/test01.ad'
+target = 'examples/test001.ad'
 
 #proc = subprocess.Popen(["leaks", "--atExit", "--", "{0}{1}".format(binary_path, binary_excutable), "{0}{1}".format(binary_path, target)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 #proc = subprocess.run(["leaks", "--atExit", "--", "{0}{1}".format(binary_path, binary_excutable), "{0}{1}".format(binary_path, target)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#proc = subprocess.Popen(["leaks", "--atExit", "--", "../../cpp/main", "examples/test01.ad"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#proc = subprocess.Popen(["leaks", "-atExit", "--", "../../cpp/main", "examples/test01.ad"], stdout=subprocess.PIPE)
-#proc = subprocess.Popen("leaks --atExit -- ../../cpp/main examples/test01.ad", shell=True)
-#proc = subprocess.Popen(["./memory-check-macos.sh", "../../cpp/main", "examples/test01.ad"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#proc = subprocess.Popen(["./memory-check-macos.sh", "../../cpp/main", "examples/test01.ad"], stdout=subprocess.PIPE)
-proc = subprocess.Popen(["./memory-check-macos.sh", "../../cpp/main", "examples/test01.ad"])
+#proc = subprocess.Popen(["leaks", "--atExit", "--", "../../cpp/main", "examples/test001.ad"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#proc = subprocess.Popen(["leaks", "-atExit", "--", "../../cpp/main", "examples/test001.ad"], stdout=subprocess.PIPE)
+#proc = subprocess.Popen("leaks --atExit -- ../../cpp/main examples/test001.ad", shell=True)
+#proc = subprocess.Popen(["./memory-check-macos.sh", "../../cpp/main", "examples/test001.ad"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#proc = subprocess.Popen(["./memory-check-macos.sh", "../../cpp/main", "examples/test001.ad"], stdout=subprocess.PIPE)
+proc = subprocess.Popen(["./memory-check-macos.sh", "../../cpp/main", "examples/test001.ad"])
 #proc = subprocess.Popen(["ls"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 proc.wait()
@@ -21,8 +21,8 @@ proc.wait()
 
 #print proc.stdout.read()
 
-#t = os.system("leaks --atExit -- ../../cpp/main examples/test01.ad")
-#t = os.popen("leaks --atExit -- ../../cpp/main examples/test01.ad").read()
+#t = os.system("leaks --atExit -- ../../cpp/main examples/test001.ad")
+#t = os.popen("leaks --atExit -- ../../cpp/main examples/test001.ad").read()
 #print t
 
 proc.communicate()
