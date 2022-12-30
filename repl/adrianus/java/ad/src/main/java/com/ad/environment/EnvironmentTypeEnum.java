94d396fffb5c3b17243e8390d929e3477d19a004
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2022-12-30 07:14:28 +0200

    [java] added EnvironmentTypeEnum

---
+++ repl/adrianus/java/ad/src/main/java/com/ad/environment/EnvironmentTypeEnum.java
@@ -0,0 +1,11 @@
+package com.ad.environment;
+
+public enum EnvironmentTypeEnum {
+    // this might be useful in the future
+    BOOTSTRAP,
+    MAIN,
+    CLASS_INSTANCE,
+    SUPER_CLASS_INSTANCE,
+    FUNCTION_CALL,
+    METHOD_CALL
+}
