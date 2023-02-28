d1c5a66deb7b6148cd602f332a188030f7ab03e7
Adrian Poplesanu <adrian.poplesanu@yahoo.com>
2023-02-28 21:43:13 +0000

    [it] added memory checks for linux runs

--- repl/adrianus/integration-tests/cpp/run-memory-checks.sh
+++ repl/adrianus/integration-tests/cpp/run-memory-checks.sh
@@ -1,6 +1,6 @@
 echo "running memory checks - run tests with platform specific memory checkers and see if there are issues" ;
 # 0 leaks for 0 total leaked bytes. trebuie cautat in output-ul memory check-ului
 
-python run-memory-checks.py ;
+python3 run-memory-checks.py ;
 
 rm mem-results.log ;
