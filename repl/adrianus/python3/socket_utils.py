d16d10cefe4e827fac01df3752b391f4196f2f15
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-08-05 17:20:08 +0300

    [python] soket utils placeholders

---
+++ repl/adrianus/python3/socket_utils.py
@@ -0,0 +1,20 @@
+def create_server():
+    pass
+
+def create_client():
+    pass
+
+def accept():
+    pass
+
+def send():
+    pass
+
+def read():
+    pass
+
+def close():
+    pass
+
+def cleanup_unescaped_characters():
+    pass
