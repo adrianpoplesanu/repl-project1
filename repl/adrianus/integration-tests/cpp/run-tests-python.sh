e5bec13af2af9c950601cf4d1ac7eb999d506b4d
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2023-08-06 14:24:25 +0300

    [it] make cpp binary run against python and java suites

---
+++ repl/adrianus/integration-tests/cpp/run-tests-python.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# echo "\033[0;36mstarting tests\033[0m" ;
+
+cp -r ../../cpp/bootstrap .
+. build-binaries.sh ;
+
+echo -e "\033[0;36mrunnning tests\033[0m" ;
+python3 run-tests-python.py ;
+echo -e "\033[0;32mfinished!\033[0m" ;
+
+echo -e "\033[0;36mrunnning memory check\033[0m" ;
+#python run-memory-checks.py ;
+. run-memory-checks-python.sh
+echo -e "\033[0;32mfinished!\033[0m" ;
+
+rm -r bootstrap/ ;
+. cleanup-tests.sh
