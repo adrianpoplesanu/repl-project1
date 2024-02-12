851af2f080a9fc59e87ed26bcace20f09ddbb541
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-02-12 22:44:54 +0200

    [java] minor cleanup in eval

--- repl/adrianus/java/ad/src/main/java/com/ad/evaluator/Evaluator.java
+++ repl/adrianus/java/ad/src/main/java/com/ad/evaluator/Evaluator.java
@@ -610,23 +610,6 @@ public class Evaluator {
     	AdFunctionObject functionObject = (AdFunctionObject) func;
      	Environment extended = EnvironmentUtils.newEnclosedEnvironment(functionObject.getEnv());
 		final int[] i = {0};
-     	/*for (AstNode param : functionObject.getParameters()) {
-			AdObject currentObject = arguments.get(i++);
-     		//extended.set(param.tokenLiteral(), arguments.get(i++));
-			// bucata asta de cod face imutabila lista cand e pasata ca argument, noua lista inca pointeaza catre vechile obiecte
-			//if (currentObject.getType() == ObjectTypeEnum.LIST) {
-			//	AdListObject oldList = (AdListObject) currentObject;
-			//	AdListObject newList = new AdListObject();
-			//	List<AdObject> elements = new ArrayList<>();
-			//	for (AdObject o : oldList.getElements()) {
-			//		elements.add(o);
-			//	}
-			//	newList.setElements(elements);
-			//	extended.setLocalParam(param.tokenLiteral(), newList);
-			//	continue;
-			//}
-			extended.setLocalParam(param.tokenLiteral(), currentObject);
-     	}*/
 		functionObject.getParameters().stream().forEach(param -> {
 			AdObject currentObject = arguments.get(i[0]++);
 			extended.setLocalParam(param.tokenLiteral(), currentObject);
@@ -638,10 +621,6 @@ public class Evaluator {
     	AdFunctionObject funcObj = (AdFunctionObject) function;
 		Environment extended = EnvironmentUtils.newEnclosedEnvironment(env);
 		final int[] i = {0};
-    	/*for (AstNode param : funcObj.getParameters()) {
-    		//env.set(param.tokenLiteral(), arguments.get(i[0]++));
-			extended.setLocalParam(param.tokenLiteral(), arguments.get(i[0]++));
-		}*/
 		funcObj.getParameters().stream().forEach(param -> extended.setLocalParam(param.tokenLiteral(), arguments.get(i[0]++)));
 		return extended;
 	}
