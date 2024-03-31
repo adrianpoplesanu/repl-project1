246a7fb70e3d598b2cb081c7431584c64c5f9e25
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-03-31 09:22:41 +0300

    [cpp] sub metrics profiling

--- repl/adrianus/cpp/profiling.cpp
+++ repl/adrianus/cpp/profiling.cpp
@@ -1,7 +1,7 @@
 #include "profiling.h"
 
-ExecutionTimeProfiling::ExecutionTimeProfiling(std::string n) {
-    name = n;
+ExecutionTimeProfiling::ExecutionTimeProfiling(std::string executionTimeProfilingName) {
+    name = executionTimeProfilingName;
     begin = 0;
     end = 0;
 }
@@ -16,4 +16,33 @@ void ExecutionTimeProfiling::stop() {
 
 void ExecutionTimeProfiling::showMetric() {
     std::cout << "[ " << name << " ] ran for: " << double(end - begin) / CLOCKS_PER_SEC << "secs\n";
+    for (const auto& entry : poi) {
+        std::cout << "[ " << name << " ] [ " << entry.first << " ] ran for: " << double(entry.second) << "secs\n";
+    }
 }
+
+void ExecutionTimeProfiling::start(const std::string& metricName) {
+    if (poi.find(metricName) != poi.end()) {
+        poi_count[metricName]++;
+        //poi_begin[metricName] = clock();
+    } else {
+        poi[metricName] = 0.0;
+        poi_count[metricName] = 1;
+        poi_begin[metricName] = clock();
+    }
+}
+
+void ExecutionTimeProfiling::stop(const std::string& metricName) {
+    if (poi.find(metricName) != poi.end()) {
+        poi_count[metricName]--;
+        if (poi_count[metricName] == 0) {
+            poi_end[metricName] = clock();
+            poi[metricName] = double(poi_end[metricName] - poi_begin[metricName]) / CLOCKS_PER_SEC;
+            //std::cout << "[ " << name << " ] [ " << metricName << " ] ran for: " << double(poi_end[metricName] - poi_begin[metricName]) / CLOCKS_PER_SEC << "secs\n";
+        }
+    } else {
+        //std::cout << metricName << " terminated";
+    }
+}
+
+ExecutionTimeProfiling executionTimeProfiling("ad");
--- repl/adrianus/cpp/profiling.h
+++ repl/adrianus/cpp/profiling.h
@@ -9,11 +9,19 @@ private:
     clock_t begin;
     clock_t end;
     std::string name;
+
+    std::unordered_map<std::string, double> poi;
+    std::unordered_map<std::string, int> poi_count;
+    std::unordered_map<std::string, clock_t> poi_begin;
+    std::unordered_map<std::string, clock_t> poi_end;
 public:
     explicit ExecutionTimeProfiling(std::string);
     void start();
     void stop();
     void showMetric();
+
+    void start(const std::string&);
+    void stop(const std::string&);
 };
 
 #endif //AD_PROFILING_H
