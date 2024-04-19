808f520166c5e9971d6e91a7a576b917677bf608
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-04-20 01:43:19 +0300

    [java] activate show running time

--- repl/adrianus/java/ad/src/main/java/Ad.java
+++ repl/adrianus/java/ad/src/main/java/Ad.java
@@ -9,7 +9,7 @@ import com.ad.parser.test.ParserTest;
 import com.ad.repl.Repl;
 
 public class Ad {
-	private static boolean showRunningTime = false;
+	private static boolean showRunningTime = true;
 
 	public static void main(String[] args) {
 		long startTime = System.nanoTime();
