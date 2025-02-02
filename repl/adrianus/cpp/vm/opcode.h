f176acdfe2d376d899f431c98f05454aa4dcd146
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-02-03 01:28:32 +0200

    [cpp] added a few more opcodes

--- repl/adrianus/cpp/vm/opcode.h
+++ repl/adrianus/cpp/vm/opcode.h
@@ -9,7 +9,30 @@ enum OpCodeType {
     OP_DIVIDE,
     OP_POP,
     OP_TRUE,
-    OP_FALSE
+    OP_FALSE,
+    OP_EQUALS,
+    OP_NOTEQUALS,
+    OP_GREATERTHAN,
+    OP_GREATERTHANEQUAL,
+    OP_MINUS,
+    OP_BANG,
+    OP_JUMP_NOT_TRUTHY,
+    OP_JUMP,
+    OP_NULL,
+    OP_GET_GLOBAL,
+    OP_SET_GLOBAL,
+    OP_ARRAY,
+    OP_HASH,
+    OP_INDEX_EXPRESSION,
+    OP_CALL,
+    OP_RETURN_VALUE,
+    OP_RETURN,
+    OP_GET_LOCAL,
+    OP_SET_LOCAL,
+    OP_GET_BUILTIN,
+    OP_CLOSURE,
+    OP_GET_FREE,
+    OP_CURRENT_CLOSURE
 };
 
 class OpCode {
@@ -70,6 +93,48 @@ class OpFalse : public OpCode {
 public:
     OpFalse() {
         byteCode = OP_FALSE;
+    }
+};
+
+class OpEquals : public OpCode {
+public:
+    OpEquals() {
+        byteCode = OP_EQUALS;
+    }
+};
+
+class OpNotEquals : public OpCode {
+public:
+    OpNotEquals() {
+        byteCode = OP_NOTEQUALS;
+    }
+};
+
+class OpGreaterThan : public OpCode {
+public:
+    OpGreaterThan() {
+        byteCode = OP_GREATERTHAN;
+    }
+};
+
+class OpGreaterThanEqual : public OpCode {
+public:
+    OpGreaterThanEqual() {
+        byteCode = OP_GREATERTHANEQUAL;
+    }
+};
+
+class OpMinus : public OpCode {
+public:
+    OpMinus() {
+        byteCode = OP_MINUS;
+    }
+};
+
+class OpBang : public OpCode {
+public:
+    OpBang() {
+        byteCode = OP_BANG;
     }
 };
 
