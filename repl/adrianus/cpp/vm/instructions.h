ca005ba247a3233f2d191a9bcfc7583e4cdea124
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-03-08 20:25:06 +0200

    [cpp] added instructions header

--- repl/adrianus/cpp/vm/instructions.h
+++ repl/adrianus/cpp/vm/instructions.h
@@ -1,4 +1,14 @@
 #ifndef __INSTRUCTIONS_H
 #define __INSTRUCTIONS_H
 
+class Instructions {
+public:
+    std::vector<unsigned char> bytes;
+
+    int size;
+    unsigned char get(int);
+    void add(unsigned char);
+    void removeLast();
+};
+
 #endif
