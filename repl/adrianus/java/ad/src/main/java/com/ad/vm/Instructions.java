db8c7c2b2498b4cdba1393bdba812f6bfc8c25ab
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-03-09 12:22:56 +0200

    [java] added Instruction entity

--- repl/adrianus/java/ad/src/main/java/com/ad/vm/Instructions.java
+++ repl/adrianus/java/ad/src/main/java/com/ad/vm/Instructions.java
@@ -1,4 +1,28 @@
 package com.ad.vm;
 
+import java.util.Arrays;
+
 public class Instructions {
+    private byte[] bytes;
+    private int n = 0;
+
+    public int size() {
+        return bytes.length;
+    }
+
+    public byte get(int i) {
+        return bytes[i];
+    }
+
+    public void add(byte instruction) {
+        bytes[n++] = instruction;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructions{" +
+                "bytes=" + Arrays.toString(bytes) +
+                ", n=" + n +
+                '}';
+    }
 }
