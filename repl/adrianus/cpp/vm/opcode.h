67350b8fd1bbeefabe45f823b96590a68220367c
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-05-04 12:49:06 +0300

    [cpp] op codes

--- repl/adrianus/cpp/vm/opcode.h
+++ repl/adrianus/cpp/vm/opcode.h
@@ -32,7 +32,20 @@ enum OpCodeType {
     OP_GET_BUILTIN,
     OP_CLOSURE,
     OP_GET_FREE,
-    OP_CURRENT_CLOSURE
+    OP_CURRENT_CLOSURE,
+
+    OP_CLASS,
+    OP_SET_METHOD,
+    OP_INSTANTIATE,
+
+    OP_GET_PROPERTY,
+    OP_SET_PROPERTY,
+
+    OP_GET_METHOD,
+    OP_INVOKE,
+
+    OP_SET_PROPERTY_SYM,
+    OP_GET_PROPERTY_SYM
 };
 
 class OpCode {
@@ -170,6 +183,153 @@ class OpSetGlobal : public OpCode {
 public:
     OpSetGlobal() {
         byteCode = OP_SET_GLOBAL;
+    }
+};
+
+class OpArray : public OpCode {
+public:
+    OpArray() {
+        byteCode = OP_ARRAY;
+    }
+};
+
+class OpHash : public OpCode {
+public:
+    OpHash() {
+        byteCode = OP_HASH;
+    }
+};
+
+class OpIndexExpression : public OpCode {
+public:
+    OpIndexExpression() {
+        byteCode = OP_INDEX_EXPRESSION;
+    }
+};
+
+class OpCall : public OpCode {
+public:
+    OpCall() {
+        byteCode = OP_CALL;
+    }
+};
+
+class OpReturnValue : public OpCode {
+public:
+    OpReturnValue() {
+        byteCode = OP_RETURN_VALUE;
+    }
+};
+
+class OpReturn : public OpCode {
+public:
+    OpReturn() {
+        byteCode = OP_RETURN;
+    }
+};
+
+class OpGetLocal : public OpCode {
+public:
+    OpGetLocal() {
+        byteCode = OP_GET_LOCAL;
+    }
+};
+    
+class OpSetLocal : public OpCode {
+public:
+    OpSetLocal() {
+        byteCode = OP_SET_LOCAL;
+    }
+};
+
+class OpGetBuiltin : public OpCode {
+public:
+    OpGetBuiltin() {
+        byteCode = OP_GET_BUILTIN;
+    }
+};
+
+class OpClosure : public OpCode {
+public:
+    OpClosure() {
+        byteCode = OP_CLOSURE;
+    }
+};
+
+class OpGetFree : public OpCode {
+public:
+    OpGetFree() {
+        byteCode = OP_GET_FREE;
+    }
+};
+
+class OpCurrentClosure : public OpCode {
+public:
+    OpCurrentClosure() {
+        byteCode = OP_CURRENT_CLOSURE;
+    }
+};
+
+class OpClass : public OpCode {
+public:
+    OpClass() {
+        byteCode = OP_CLASS;
+    }
+};
+
+class OpSetMethod : public OpCode {
+public:
+    OpSetMethod() {
+        byteCode = OP_SET_METHOD;
+    }
+};
+
+class OpInstantiate : public OpCode {
+public:
+    OpInstantiate() {
+        byteCode = OP_INSTANTIATE;
+    }
+};
+
+class OpGetProperty : public OpCode {
+public:
+    OpGetProperty() {
+        byteCode = OP_GET_PROPERTY;
+    }
+};
+    
+class OpSetProperty : public OpCode {
+public:
+    OpSetProperty() {
+        byteCode = OP_SET_PROPERTY;
+    }
+};
+
+class OpGetMethod : public OpCode {
+public:
+    OpGetMethod() {
+        byteCode = OP_GET_METHOD;
+    }
+};
+    
+class OpInvoke : public OpCode {
+public:
+    OpInvoke() {
+        byteCode = OP_INVOKE;
+    }
+};
+
+class OpSetPropertySym : public OpCode {
+public:
+    OpSetPropertySym() {
+        byteCode = OP_SET_PROPERTY_SYM;
+    }
+};
+
+class OpGetPropertySym : public OpCode {
+public:
+    OpGetPropertySym() {
+        byteCode = OP_GET_PROPERTY_SYM;
     }
 };
 
