85ba80150ce8e81231c6edcb8463ad1b40757b52
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-05-23 00:51:31 +0300

    [cpp] compiler and vm placeholders

--- repl/adrianus/cpp/vm/compiler.h
+++ repl/adrianus/cpp/vm/compiler.h
@@ -1,4 +1,9 @@
 #ifndef __COMPILER_H
 #define __COMPILER_H
 
+class Compiler {
+public:
+    Compiler();
+};
+
 #endif
--- repl/adrianus/cpp/vm/vm.h
+++ repl/adrianus/cpp/vm/vm.h
@@ -1,4 +1,9 @@
 #ifndef __VM_H
 #define __VM_H
 
+class VM {
+public:
+    VM();
+};
+
 #endif
