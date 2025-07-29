b6531286fea04369476db14055986acab445e8d8
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-07-30 00:37:25 +0300

    [python] updte print and println builtins to use objects repr()

--- repl/adrianus/python3/builtin_funcs.py
+++ repl/adrianus/python3/builtin_funcs.py
@@ -28,12 +28,12 @@ def first_builtin(args, env):
 def print_builtin(args, env):
     for obj in args:
         if obj:
-            print (obj.inspect(), end='')
+            print(obj.repr(), end='')
 
 def println_builtin(args, env):
     for obj in args:
         if obj:
-            print (obj.inspect())
+            print(obj.repr())
 
 def exit_builtin(args, env):
     sys.exit(0)
