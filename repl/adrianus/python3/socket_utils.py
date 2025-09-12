bd72b928216a755100a7bbfc7f12d9af02c79ddd
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-09-12 13:37:02 +0300

    [python] socket utils refactoring

--- repl/adrianus/python3/socket_utils.py
+++ repl/adrianus/python3/socket_utils.py
@@ -1,33 +1,41 @@
 import socket
+import re
 from objects import Ad_Socket_Object, Ad_String_Object
 
-def create_server(socket_obj):
+
+def create_server(raw_socket: Ad_Socket_Object):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind(('127.0.0.1', 5003))
     sock.listen()
-    socket_obj.sock = sock
+    raw_socket.sock = sock
 
-def create_client(socket_obj):
+
+def create_client(raw_socket: Ad_Socket_Object):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect(('127.0.0.1', 5003))
-    socket_obj.sock = sock
+    raw_socket.sock = sock
 
-def accept_socket(socket_obj):
-    conn, addr = socket_obj.sock.accept()
+
+def accept_socket(raw_socket: Ad_Socket_Object):
+    conn, addr = raw_socket.sock.accept()
     client_sock = Ad_Socket_Object(name="client", host="127.0.0.1", port=5003, is_active=True,
             is_forever=False, is_client=True, is_server=False, sock=None,
             conn=conn, addr=addr)
     return client_sock
 
-def send_socket(socket_obj, message):
-    escaped_text = cleanup_unescaped_characters(message.value)
-    if socket_obj.conn:
-        socket_obj.conn.sendall(escaped_text.encode())
 
-def read_socket(socket_obj):
+def send_socket(raw_socket: Ad_Socket_Object, raw_string: Ad_String_Object):
+    escaped_text = cleanup_unescaped_characters(raw_string.value)
+    if raw_socket.conn:
+        raw_socket.conn.sendall(escaped_text.encode())
+
+
+def read_socket(raw_socket: Ad_Socket_Object):
     message = ""
     while True:
-        data = socket_obj.sock.recv(1024)
+        if not raw_socket.sock:
+            break
+        data = raw_socket.sock.recv(1024)
         if data:
             message += data.decode()
         else:
@@ -35,10 +43,28 @@ def read_socket(socket_obj):
     result = Ad_String_Object(value=message)
     return result
 
-def close_socket(socket_obj):
-    if socket_obj and socket_obj.conn:
-        socket_obj.conn.close()
 
-def cleanup_unescaped_characters(raw_text):
+def read_http(raw_socket: Ad_Socket_Object):
+    message = ""
+    while True:
+        if not raw_socket.sock:
+            break
+        data = raw_socket.sock.recv(1024)
+        if data:
+            message += data.decode()
+        else:
+            break
+    result = Ad_String_Object(value=message)
+    return result
+
+
+def close_socket(raw_socket: Ad_Socket_Object):
+    if raw_socket and raw_socket.conn:
+        raw_socket.conn.close()
+
+
+def cleanup_unescaped_characters(raw_text: str):
     # TODO: fix this
-    return raw_text
+    escaped_text = re.sub('\\\\r', '\r', raw_text)
+    escaped_text = re.sub('\\\\n', '\n', escaped_text)
+    return escaped_text
