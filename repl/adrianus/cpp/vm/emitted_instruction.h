8345e143c7ab353382541a34c4fd1d3ee5a975ce
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2025-10-17 00:22:28 +0300

    [cpp] emitted instructions

--- repl/adrianus/cpp/vm/emitted_instruction.h
+++ repl/adrianus/cpp/vm/emitted_instruction.h
@@ -1,4 +1,24 @@
 #ifndef AD_VM_EMITTED_INSTRUCTION_H
 #define AD_VM_EMITTED_INSTRUCTION_H
 
+#include "opcode.h"
+
+class EmittedInstruction {
+private:
+    OpCodeType opcode;
+    int position;
+
+public:
+    // Constructor with default parameters (similar to Python __init__)
+    EmittedInstruction(OpCodeType opcode = OP_CONSTANT, int position = -1);
+    
+    // Getters
+    OpCodeType getOpcode() const;
+    int getPosition() const;
+    
+    // Setters
+    void setOpcode(OpCodeType opcode);
+    void setPosition(int position);
+};
+
 #endif
